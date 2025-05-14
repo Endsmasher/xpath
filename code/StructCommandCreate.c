452a5593a99a8837e107d2462a2fe90e4af707ce
MichaelLadisch <michiladisch@gmail.com>
2025-05-14 15:17:43 +0200

    updated code

--- code/StructCommandCreate.c
+++ code/StructCommandCreate.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 //#include <helper.h>
+//#include <path_element.h>
 
 // ./Programm "C:\\Users\\herbi\\CLionProjects\\xpath\\html\\test.txt div/div/div/price/id=223221/Auto"
+// absolute path nötig aktuell
 
 
 void Programm(){
     // für denn file path
     printf("Programm start\n");
     printf("Bitte Zuerst denn dateipfad und dann denn path. Mit leerzeichen trennen: ");
-    char eingabe[200];
+    char eingabe[200];  // noch ändern auf ohne limit
     fgets(eingabe, sizeof(eingabe)-1, stdin);  //fgets liest alles ein inklusive leerzeichen
     char *file_path = strtok(eingabe, " ");
     printf("File-Path: %s\n", file_path);
@@ -19,14 +21,13 @@ void Programm(){
     char *token = strtok(NULL, "/");
     while (token != NULL) {
         int id;
-        printf("Element-Pfad: %s\n", token);
         if (sscanf(token, "id=%d", &id) == 1) {   // scant token, erwarte I,D,= und ließt eine zahl ( %d ) und speichert die in Id
             printf("Element-Pfad id: %d\n", id);
-            token = strtok(NULL, "/");
         }
         else {
-            token = strtok(NULL, "/");
+            printf("Element-Pfad: %s\n", token);
         }
+        token = strtok(NULL, "/");
     }
 }
 int main() {
