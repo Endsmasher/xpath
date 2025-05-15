3a16893068375234f8da8cfd1a37d9f1b1807b26
Endsmasher <endsmeshaa@gmail.com>
2025-05-15 17:49:31 +0200

    add create path comment

--- code/path_element.h
+++ code/path_element.h
@@ -15,6 +15,13 @@ typedef struct PathElement {
   int children_count;
 } PathElement;
 
+/*
+ * create path element tree structure
+ * type the type of the html element (div, body, header)
+ * id array of strings containing the ids (custom css id tags specific to each website)
+ * class array of string containing the classes of the html element
+ * parent the parent object to properly connect the elements
+ */
 PathElement *createPathElement(char *type, String id[], int id_count, String class[], int class_count, PathElement *parent);
 
 void freePathElements(PathElement *element);
